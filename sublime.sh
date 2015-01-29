0fb8153142e5e8737affaf139aa39a880c25ecd6
Andrii Lundiak <landike@gmail.com>
2015-01-29 14:19:52 +0200

    A bit modified and added WhoCalled plugin

--- sublime.sh
+++ sublime.sh
@@ -17,7 +17,12 @@ git clone https://github.com/Xavura/CoffeeScript-Sublime-Plugin.git "$PACKAGES/C
 git clone https://github.com/aponxi/sublime-better-coffeescript.git "$PACKAGES/Better CoffeeScript";
 
 # Plugin to reformat code by pressing Shift+Cmd+H
+# https://packagecontrol.io/packages/HTML-CSS-JS%20Prettify
 git clone https://github.com/victorporof/Sublime-HTMLPrettify.git "$PACKAGES/Sublime-HTMLPrettify";
 
 # AngularJS plugin (code completion, snippets, go to definition, quick panel search)
 git clone git://github.com/angular-ui/AngularJS-sublime-package.git "$PACKAGES/AngularJS";
+
+# Looks like Function definition search/click/jump plugin
+# https://packagecontrol.io/packages/WhoCalled%20Function%20Finder
+git clone https://bitbucket.org/rablador/whocalled.git "$PACKAGES/WhoCalled";
