fcb0522e94a35a121d7088a5abfca779967ef3d9
Andrii Lundiak <landike@gmail.com>
2020-10-19 15:43:16 +0200

    Small code cleanup

--- clean.sh
+++ clean.sh
@@ -4,7 +4,7 @@ TO_DELETE="node_modules"
 
 for ENTRY in `find ../  -name "$TO_DELETE" -prune -depth +1 -print`
 do
-    echo "Entities $TO_DELETE are being deleted..."
+    echo "$ENTRY is being deleted..."
     rm -rf $ENTRY
-    echo "Entities $TO_DELETE have been deleted..."
+    echo "$ENTRY has been deleted..."
 done
