02a233f9272f574b54f51e1b90a170d88a89fd97
Andrii Lundiak <landike@gmail.com>
2021-05-05 22:13:13 +0200

    OK, so it's not only npm and yarn cache, but all possible cache on MacOS. Very handy, when I am running out of space, and script cleans ~10 GB :)

---
+++ clean.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+TO_DELETE="node_modules"
+
+for ENTRY in `find ../  -name "$TO_DELETE" -prune -depth +1 -print`
+do
+    echo "$ENTRY is being deleted..."
+    rm -rf $ENTRY
+    echo "$ENTRY has been deleted..."
+done
+
+echo "Clearing NPM cache..."
+npm cache clean --force
+
+echo "Clearing NPX cache..."
+# https://stackoverflow.com/questions/63510325/how-can-i-clear-the-central-cache-for-npx
+rm -rf ~/.npm/_npx
+
+# https://yarnpkg.com/cli/cache/clean
+# https://classic.yarnpkg.com/en/docs/cli/cache
+yarn cache list
+echo "Clearing Yarn cache..."
+yarn cache clean --all
+
+# Cleanup cached fiels by HomeBrew
+# https://www.educative.io/edpresso/what-is-brew-cleanup
+echo "Cleaning by HomeBrew..."
+brew cleanup
+
+# https://github.com/Homebrew/brew/issues/3784
+echo "Clearing cached files from $(brew --cache) ..."
+rm -rf $(brew --cache)
+
+echo "Cleaning what left from Python/pip ..."
+# rm -rf ~/Library/Caches/pip # doens't exists for me
+#
+# https://stackoverflow.com/questions/9510474/removing-pips-cache
+pip3 cache list
+# pip cache dir
+# pip3 cache dir # => /Users/alund/Library/Caches/pip
+pip3 cache purge
+
+# echo "Clearing MacOS system cache ..."
+# sudo rm -rf ~/Library/Caches/**
+# since 2014-2017-2021 I got ~10GB of files there.
+# So in 2021 I manually renamed fodler, and newlly aut-recreated Cache fodler got ~
+# not permitted EVEN with sudo, so need to delete manually
+
+echo "Clearing MacOS user cache ..."
+rm -rf ~/.cache/**
+
+echo "Clearing Dropbox logs user cache ..."
+rm -rf ~/.dropbox/logs/**
+
+echo "Clearing Cisco VPN user cache and logs ..."
+rm -rf ~/.cisco/vpn/cache/**
+rm -rf ~/.cisco/vpn/log/**
+
+echo "Clearing 'not sure whose' (potentially Ruby) ~/.bundle cache ..."
+rm -rf ~/.bundle/cache/**
+
+echo "Clearing Anaconda temp files ..."
+rm -rf ~/.anaconda/navigator/temp/**
+
+echo "Clearing RN(ReactNative?) temp files ..."
+rm -rf ~/.rncache/**
+
